3a87491152bdb7bd0607da3c57d564c8f784ed12
Gody <adekaji@gmail.com>
2023-03-15 10:07:03 +0000

    update

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -12,16 +12,17 @@ def rotate_2d_matrix(matrix):
     Return:
         None
     """
-    n = len(matrix)
-    for i in range(n):
-        for j in range(i):
-            temp = matrix[i][j]
-            matrix[i][j] = matrix[j][i]
-            matrix[j][i] = temp
+    N = len(matrix)
 
-    for i in range(n):
-        for j in range(int(n / 2)):
+    # transpose matrix
+    for i in range(N):
+        for j in range(i):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # reverse matrix
+    for i in range(N):
+        for j in range(N//2):
             temp = matrix[i][j]
-            matrix[i][j] = matrix[i][n-1-j]
-            matrix[i][n-1-j] = temp
+            matrix[i][j] = matrix[i][N-j-1]
+            matrix[i][N-j-1] = temp
 
